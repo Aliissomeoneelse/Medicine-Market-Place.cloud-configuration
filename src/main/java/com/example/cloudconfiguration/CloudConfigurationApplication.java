60f9049f12f3f6ba5dc3d596eafd6d85e0b8a2b1
Alisher Khudoyberdiev <aliwulug1307@gmail.com>
2023-08-21 15:51:24 +0500

    cloud-config added

---
+++ src/main/java/com/example/cloudconfiguration/CloudConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigurationApplication.class, args);
+	}
+
+}
